4202717601abcd34e795abd1bb45977c536e19e2
AnasCoding1986 <anassust.scw@gmail.com>
2024-03-24 13:14:59 +0600

    module 4 completed

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,11 +1,22 @@
 
 
 const Job = ({job}) => {
-    const {logo} = job;
+    const {id,logo,job_title,company_name,remote_or_onsite,location,job_type,salary,job_description,job_responsibility,educational_requirements,experiences,contact_information} = job;
     return (
-        <div>
-            <img src={logo} alt="" />
-        </div>
+        <div className="card card-compact bg-base-100 shadow-xl">
+  <figure><img src={logo} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{job_title}</h2>
+    <p>{company_name}</p>
+    <div>
+        <button className="mr-4 px-7 py-4 text-blue-400 font-extrabold rounded-2xl border border-cyan-500 my-4">{remote_or_onsite}</button>
+        <button className="mr-4 px-7 py-4 text-blue-400 font-extrabold rounded-2xl border border-cyan-500 my-4">{job_type}</button>
+    </div>
+    <div className="card-actions">
+      <button className="btn btn-primary">View DEtails</button>
+    </div>
+  </div>
+</div>
     );
 };
 
